7040509ae950c68ce07b9e29196cd24bb25c894c
SNP-0309 <srujanpoojari2@gmail.com>
2026-07-12 15:14:42 +0530

    adding scraper for better search

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: '@react-native/jest-preset',
+  setupFiles: ['./jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation|@react-navigation/.*|@react-native-async-storage)/)',
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@react-navigation/.*|@react-native-async-storage|react-native-gesture-handler|react-native-reanimated|react-native-screens|react-native-vector-icons|lottie-react-native)/)',
   ],
 };
